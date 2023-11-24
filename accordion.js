73b3fc6c742fbc0538d90d77ba23561a37cd3217
ashleymuffin <147425756+ashleymuffin@users.noreply.github.com>
2023-11-24 22:44:37 +0530

    Add files via upload

---
+++ accordion.js
@@ -0,0 +1,11 @@
+(function ($) {
+    'use strict';
+    $('.item').on("click", function () {
+        $(this).next().slideToggle(100);
+        $('p').not($(this).next()).slideUp('fast');
+    });
+}(jQuery));
+
+//
+
+
